2cbacee7669e5a5a6e8c276aa115c95ba4b369d4
666wcy <769020367@qq.com>
2020-01-01 14:54:54 +0800

    登录方式改变，已更新
    2020年1月1日，网站登录方式改变，已更新，测试正常

--- budang.py
+++ budang.py
@@ -7,22 +7,23 @@ admin='账号'
 password='密码'
 
 if __name__ == '__main__':
-
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 5.4; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
 
     }
     url='https://www.manhuabudang.com/index.php'        #主页面地址
     html=requests.get(url,headers=header)
-    #开始获取模拟登陆需要的post数据
-    step=re.findall("<input value=\"(.)\" type=\"hidden\" name=\"step\">", html.text, re.S)[0]
-    print(step)
+    #开始获取模拟登陆需要的post数据-step
+    step=re.findall("<input type=\"hidden\" name=\"step\" value=\"(.*?)\" />", html.text, re.S)[0]
+    bbs=re.findall("<input type=\"hidden\" name=\"(.*?)\" value=\"bbs\" />", html.text, re.S)[0]
+
     data={
        'pwuser':admin,
         'pwpwd':password,
         'cktime':'3600',
         'jumpurl':'https://www.manhuabudang.com/index.php',
         'step':step,
+        "bbs":bbs,
         'question': '0',
         'lgt': '0',
         'hideid': '0'
